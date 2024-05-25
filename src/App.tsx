52fb70bf13c2e8c1ee5bbe91712ab6d4a4391241
Danir <davaleev@edu.hse.ru>
2024-05-25 22:06:28 +0100

    imstupid

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
+import logo from './logo-white.svg'
 import WebApp from '@twa-dev/sdk'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const [isClicked, setIsClicked] = useState(false);
+  const handleMouseDown = () => {
+    setIsClicked(true);
+    setCount(count + 1);
+  };
+  const handleMouseUp = () => {
+    setIsClicked(false);
+  };
+  useEffect(() => {
+    document.body.style.backgroundColor = "#FD54A6";
+  }, []);
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-      <div className="card">
-        <button onClick={() => WebApp.showAlert(`Hello World! Current count is ${count}`)}>
-          Show Alert
-        </button>
-      </div>
-    </>
-  )
-}
+    <div className="container">
+      <span className="counter">{count}</span>
+      <button onMouseDown={handleMouseDown} onMouseUp={handleMouseUp} className="button">
+        <img src={logo} alt="logo" style={{
+          filter: isClicked ? 'none' : 'drop-shadow(0 10px 20px rgba(0, 0, 0, 0.2)) drop-shadow(0 4px 8px rgba(0, 0, 0, 0.2))',
+          backgroundColor: 'transparent'
+        }} />
+      </button>
+    </div>
+  );
+};
 
 export default App
