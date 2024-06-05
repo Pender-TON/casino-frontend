1bc98c0c40331685c3570d046086f0b21a87d63b
Danir <davaleev@edu.hse.ru>
2024-06-05 17:32:37 +0100

    allignment

--- src/TableTopData.tsx
+++ src/TableTopData.tsx
@@ -8,11 +8,13 @@ interface TableTopDataProps {
 
 const TableTopData: React.FC<TableTopDataProps> = ({ displayCount, displayGems, leaderboardPosition, imageSrc }) => {
     return (
-        <div className="flex flex-row justify-center items-center gap-x-4" >
-            <img src={imageSrc} alt="Tabletop" className="w-16 h-16" />
-            <div className="flex flex-col items-center justify-center space-y-0">
-                <span className="text-4xl tabular-nums text-white select-none">{displayCount.toLocaleString()}</span>
-                <span className="text-4xl tabular-nums text-white select-none">{displayGems.toLocaleString()}</span>
+        <div className="flex flex-row justify-between items-center gap-x-4 mt-16 w-full" >
+            <div className="flex flex-row items-center gap-x-4">
+                <img src={imageSrc} alt="Tabletop" className="w-16 h-16" />
+                <div className="flex flex-col items-center justify-center space-y-0">
+                    <span className="text-4xl tabular-nums text-white select-none">{displayCount.toLocaleString()}</span>
+                    <span className="text-4xl tabular-nums text-white select-none">{displayGems.toLocaleString()}</span>
+                </div>
             </div>
             <span className="text-4xl tabular-nums text-white select-none">{`${leaderboardPosition}`}</span>
         </div>
