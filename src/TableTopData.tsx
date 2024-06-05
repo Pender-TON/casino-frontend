8f8118d20d27442659abc34a4bb2ff03d0b61413
Danir <davaleev@edu.hse.ru>
2024-06-05 19:57:58 +0100

    d alignment

--- src/TableTopData.tsx
+++ src/TableTopData.tsx
@@ -13,8 +13,8 @@ const TableTopData: React.FC<TableTopDataProps> = ({ displayCount, displayGems, 
         <div className="flex flex-row justify-between items-center gap-x-4 mt-4 mx-4 px-11 w-full" >
             <div className="flex flex-row items-center gap-x-4">
                 <img src={imageSrc} alt="Tabletop" className="w-19 h-19" />
-                <div className="flex flex-col items-center justify-center space-y-0">
-                    <div className="flex flex-col items-center gap-y-2">
+                <div className="flex flex-col justify-center space-y-0">
+                    <div className="flex items-start gap-y-2">
                         <div className="flex items-center gap-x-2">
                             <img src={SmallChip} alt="Chip" className="w-6 h-6" />
                             <span className="text-3xl tabular-nums text-white select-none">{displayCount.toLocaleString()}</span>
