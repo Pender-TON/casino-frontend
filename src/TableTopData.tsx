c2484ef22ede5a9b503a1afb12ea72c86b19b427
Danir <davaleev@edu.hse.ru>
2024-06-05 20:08:30 +0100

    image fix

--- src/TableTopData.tsx
+++ src/TableTopData.tsx
@@ -13,7 +13,7 @@ const TableTopData: React.FC<TableTopDataProps> = ({ displayCount, displayGems, 
     return (
         <div className="flex flex-row justify-between items-center gap-x-4 mt-4 mx-4 px-11 w-full" >
             <div className="flex flex-row items-center gap-x-4">
-                <img src={imageSrc} alt="Tabletop" className="w-32 h-32" />
+                <img src={imageSrc} alt="Tabletop" className="w-18 h-18" />
                 <div className="flex flex-col justify-center space-y-0">
                     <div className="flex items-center gap-x-2">
                         <img src={SmallChip} alt="Chip" className="w-6 h-6" />
