aebf61b61bc204e0f51c7d1277f238da266f965e
Danir <davaleev@edu.hse.ru>
2024-06-05 17:49:39 +0100

    small-chip

--- src/TableTopData.tsx
+++ src/TableTopData.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ReactComponent as SmallChip } from './assets/small-chip.svg';
 interface TableTopDataProps {
     displayCount: number;
     displayGems: number;
@@ -8,11 +9,14 @@ interface TableTopDataProps {
 
 const TableTopData: React.FC<TableTopDataProps> = ({ displayCount, displayGems, leaderboardPosition, imageSrc }) => {
     return (
-        <div className="flex flex-row justify-between items-center gap-x-4 mt-4 mx-4 px-8 w-full" >
+        <div className="flex flex-row justify-between items-center gap-x-4 mt-4 mx-4 px-15 w-full" >
             <div className="flex flex-row items-center gap-x-4">
-                <img src={imageSrc} alt="Tabletop" className="w-18 h-18" />
+                <img src={imageSrc} alt="Tabletop" className="w-19 h-19" />
                 <div className="flex flex-col items-center justify-center space-y-0">
-                    <span className="text-3xl tabular-nums text-white select-none">{displayCount.toLocaleString()}</span>
+                    <div className="flex items-center gap-x-2">
+                        <SmallChip />
+                        <span className="text-3xl tabular-nums text-white select-none">{displayCount.toLocaleString()}</span>
+                    </div>
                     <span className="text-3xl tabular-nums text-white select-none">{displayGems.toLocaleString()}</span>
                 </div>
             </div>
