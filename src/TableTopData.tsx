5ac38032ee60cd1c2ffa43814af6d58ff33b8271
Danir <davaleev@edu.hse.ru>
2024-06-05 17:41:12 +0100

    sizing_p

--- src/TableTopData.tsx
+++ src/TableTopData.tsx
@@ -8,9 +8,9 @@ interface TableTopDataProps {
 
 const TableTopData: React.FC<TableTopDataProps> = ({ displayCount, displayGems, leaderboardPosition, imageSrc }) => {
     return (
-        <div className="flex flex-row justify-between items-center gap-x-4 mt-4 mx-6 px-4 w-full" >
+        <div className="flex flex-row justify-between items-center gap-x-4 mt-4 mx-4 px-8 w-full" >
             <div className="flex flex-row items-center gap-x-4">
-                <img src={imageSrc} alt="Tabletop" className="w-20 h-20" />
+                <img src={imageSrc} alt="Tabletop" className="w-18 h-18" />
                 <div className="flex flex-col items-center justify-center space-y-0">
                     <span className="text-3xl tabular-nums text-white select-none">{displayCount.toLocaleString()}</span>
                     <span className="text-3xl tabular-nums text-white select-none">{displayGems.toLocaleString()}</span>
