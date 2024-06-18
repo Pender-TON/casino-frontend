378a175aa350901efde233fb0d6afc4c0daad3e9
Sunny Yadav <blinded@list.ru>
2024-06-18 06:41:13 +0300

    test agin

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.DEV ? '/api' : import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API,
   timeout: 10000,
 });
 
