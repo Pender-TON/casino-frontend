a107d72a84f4949ebea7f2bba5e9f7d8512f4eac
Sunny Yadav <blinded@list.ru>
2024-06-20 00:08:21 +0300

    moves ton manifest to public

--- src/main.tsx
+++ src/main.tsx
@@ -23,7 +23,7 @@ const queryClient = new QueryClient({
 smoothscroll.polyfill();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <TonConnectUIProvider manifestUrl="https://pender-lwydwhnl2-pender-mvp.vercel.app/tonconnect-manifest.json">
+  <TonConnectUIProvider manifestUrl="https://pender-tma.vercel.app/tonconnect-manifest.json">
     <React.StrictMode>
       <QueryClientProvider client={queryClient}>
         <App />
